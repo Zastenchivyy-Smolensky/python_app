70d34bae02ea66e1557212030f8351f9797d41ab
ryouta <89526043+Zastenchivyy-Smolensky@users.noreply.github.com>
2021-11-24 22:48:52 +0900

    end CRUD and find method

--- hello/urls.py
+++ hello/urls.py
@@ -1,5 +1,13 @@
-from django.conf.urls import url
-from .views import HelloView
+from django.urls import path
+from . import views
+from .views import FriendList
+from .views import FriendDetail
 urlpatterns = [
-    url(r'',HelloView.as_view(), name="index"),
+    path('',views.index,name="index"),
+    path("create",views.create,name="create"),
+    path("edit/<int:num>",views.edit,name="edit"),
+    path("delete/<int:num>",views.delete,name="delete"),
+    path('list',FriendList.as_view()),
+    path("detail/<int:pk>",FriendDetail.as_view()),
+    path("find",views.find,name="find"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,77 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from django.shortcuts import redirect
+from .models import Friend
+from .forms import FriendForm
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from .forms import FindForm
+from django.db.models import Q
 
-class HelloView(TemplateView):
-    def __init__(self):
-        self.params={
-            "title":"hello",
-            "message":"your data:",
-            "form":HelloForm()
-        }
+class FriendList(ListView):
+    model=Friend
+class FriendDetail(DetailView):
+    model=Friend
+
+def index(request):
+    data=Friend.objects.all().order_by("age")
+    params={
+        "title":"hello",
+        "data":data,
+    }
     
-    def get(self, request):
-        return render(request, "hello/index.html",self.params)
-    
-    def post(self,request):
-        msg="あなたは<b>"+request.POST["name"]+\
-            "("+request.POST["age"]+")</b>さんです"+\
-                "メールアドレスは<b>"+request.POST["mail"]+"</b>ですね"
-        self.params["message"]=msg
-        self.params["form"]=HelloForm(request.POST)
-        return render(request, "hello/index.html", self.params)
+    return render(request,"hello/index.html", params)
+
+def create(request):
+    if(request.method == "POST"):
+        obj=Friend()
+        friend = FriendForm(request.POST,instance=obj)
+        friend.save()
+        return redirect(to="/hello")
+    params={
+        "title":"hello",
+        "form":FriendForm(),
+    }
+    return render(request, "hello/create.html",params)
+
+def edit(request,num):
+    obj=Friend.objects.get(id=num)
+    if(request.method=="POST"):
+        friend=FriendForm(request.POST,instance=obj)
+        friend.save()
+        return redirect(to="/hello")
+    params={
+        "title":"hello",
+        "id":num,
+        "form":FriendForm(instance=obj),
+    }
+    return render(request, "hello/edit.html",params)
+def delete(request,num):
+    friend=Friend.objects.get(id=num)
+    if(request.method=="POST"):
+        friend.delete()
+        return redirect(to="/hello")
+    params={
+        "title":"hello",
+        "id":num,
+        "obj":friend,
+    }
+    return render(request, "hello/delete.html", params)
+def find(request):
+    if(request.method=="POST"):
+        form=FindForm(request.POST)
+        find=request.POST["find"]
+        list=find.split()
+        data=Friend.objects.filter(name__in=list)
+        msg="Result:"+str(data.count())
+    else:
+        msg="serch words..."
+        form=FindForm()
+        data=Friend.objects.all()
+    params={
+        "title":"hello",
+        "message":msg,
+        "form":form,
+        "data":data,
+    }
+    return render(request, "hello/find.html", params)
