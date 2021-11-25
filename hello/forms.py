b1884ae349d96494cb83dd9c5ad3443500852385
ryouta <89526043+Zastenchivyy-Smolensky@users.noreply.github.com>
2021-11-26 00:23:29 +0900

    pagenator finish

--- hello/forms.py
+++ hello/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from.models import Friend
-
+class CheckForm(forms.Form):
+    
+    str=forms.CharField(label="String",\
+        widget=forms.TextInput(attrs={"class":"form-control"}))
+    def clean(self):
+        cleaned_data=super().clean()
+        str=cleaned_data["str"]
+        if(str.lower().startswith('no')):
+            raise forms.ValidationError('You input "NO"!')
 class FindForm(forms.Form):
     find = forms.CharField(label="Find",required=False,\
         widget=forms.TextInput(attrs={"class":"form-control"}))
@@ -8,7 +16,12 @@ class FriendForm(forms.ModelForm):
     class Meta:
         model=Friend
         fields=["name","mail","gender","age","birthday"]
-        
+        params={
+            "name":forms.TextInput(attrs={"class":"form-control"}),
+            "mail":forms.EmailInput(attrs={"class":"form-control"}),
+            "age":forms.NumberInput(attrs={"class":"form-control"}),
+            "birthday":forms.DateInput(attrs={"class":"form-control"}),
+        }
 class HelloForm(forms.Form):
     name=forms.CharField(label="Name",\
         widget=forms.TextInput(attrs={"class":"form-control"}))
--- hello/views.py
+++ hello/views.py
@@ -8,30 +8,37 @@ from django.views.generic import DetailView
 from .forms import FindForm
 from django.db.models import Q
 from django.db.models import Count,Sum,Avg,Min,Max
-
+from .forms import CheckForm
+from django.core.paginator import Paginator
 class FriendList(ListView):
     model=Friend
 class FriendDetail(DetailView):
     model=Friend
 
-def index(request):
+def index(request,num=1):
     data=Friend.objects.all()
-    re1=Friend.objects.aggregate(Count("age"))
-    re2=Friend.objects.aggregate(Sum("age"))
-    re3=Friend.objects.aggregate(Avg("age"))
-    re4=Friend.objects.aggregate(Min("age"))
-    re5=Friend.objects.aggregate(Max("age"))
-    msg="count:"+str(re1["age__count"])\
-        +"<br>Sum:"+str(re2["age__sum"])\
-            +"<br>Average:"+str(re3["age__avg"])\
-                +"<br>最小値:"+str(re4["age__min"])\
-                    +"<br>最大値:"+str(re5["age__max"])
-        
+    page=Paginator(data,3)
     params={
         "title":"hello",
-        "message":msg,
-        "data":data,
+        "message":"",
+        "date":page.get_page(num),
     }
+    # re1=Friend.objects.aggregate(Count("age"))
+    # re2=Friend.objects.aggregate(Sum("age"))
+    # re3=Friend.objects.aggregate(Avg("age"))
+    # re4=Friend.objects.aggregate(Min("age"))
+    # re5=Friend.objects.aggregate(Max("age"))
+    # msg="count:"+str(re1["age__count"])\
+    #     +"<br>Sum:"+str(re2["age__sum"])\
+    #         +"<br>Average:"+str(re3["age__avg"])\
+    #             +"<br>最小値:"+str(re4["age__min"])\
+    #                 +"<br>最大値:"+str(re5["age__max"])
+        
+    # params={
+    #     "title":"hello",
+    #     "message":msg,
+    #     "data":data,
+    #}
     
     return render(request,"hello/index.html", params)
 
@@ -88,3 +95,19 @@ def find(request):
         "data":data,
     }
     return render(request, "hello/find.html", params)
+
+def check(request):
+    params={
+        "title":"hello",
+        "message":"check validation",
+        "form":FriendForm(),
+    }
+    if(request.method=="POST"):
+        obj=Friend()
+        form=FriendForm(request.POST,instance=obj)
+        params["form"]=form
+        if(form.is_valid()):
+            params["message"]="OK"
+        else:
+            params["message"]="no good"
+    return render(request, "hello/check.html" , params)
