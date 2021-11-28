a5e7b87a41d530ff51264cedae85b6c5ed503123
ryouta <89526043+Zastenchivyy-Smolensky@users.noreply.github.com>
2021-11-28 10:34:11 +0900

    finish views

--- sns/views.py
+++ sns/views.py
@@ -22,7 +22,7 @@ def index(request,page=1):
         checkform=GroupCheckForm(request.user)
         gps=Group.objects.filter(owner=request.user)
         glist=[public_group.title]
-        for imte in gps:
+        for item in gps:
             glist.append(item.title)
         messages=get_your_group_message(request.user,glist,page)
     params={
@@ -49,12 +49,13 @@ def groups(request):
             groupform=GroupSelectForm(request.user,request.POST)
             friendsform=FriendsForm(request.user,\
                 friends=friends,vals=vlist)
+            
             if request.POST["mode"]=="__friends_form__":
                 sel_group=request.POST["group"]
                 group_obj=Group.objects.filter(title=sel_group).first()
-                print(grop_obj)
-                sel_fdsrequet.POST.getlist("friends")
-                sel_users=User.objects.filter(username__in=self_fds)
+                print(group_obj)
+                sel_fds=request.POST.getlist("friends")
+                sel_users=User.objects.filter(username__in=sel_fds)
                 fds=Friend.objects.filter(owner=request.user)\
                     .fliter(user__in=sel_users)
                 vlist=[]
@@ -62,7 +63,7 @@ def groups(request):
                     item.group=group_obj
                     item.save()
                     vlist.append(item.user.username)
-                messsage.success(request,"チェックされたFriendを"+\
+                messages.success(request,"チェックされたFriendを"+\
                     sel_group+"に登録しました")
                 groupform=GroupSelectForm(request.user,\
                     {"groups":sel_group})
@@ -77,7 +78,7 @@ def groups(request):
     params={
         "login_user":request.user,
         "friends_form":groupsform,
-        "friends_form":friendforms,
+        "friends_form":friendsform,
         "create_form":createform,
         "group":sel_group,
     }
@@ -94,7 +95,7 @@ def add(request):
     frd.user=add_user
     frd.group=public_group
     frd.save()
-    messag.success(request,add_user.username+"を追加しました!groupページに移動して,追加したFriendをメンバーに設定してください")
+    messages.success(request,add_user.username+"を追加しました!groupページに移動して,追加したFriendをメンバーに設定してください")
     return redirect(to="/sns")
 @login_required(login_url="/admin/login/")
 def creategroup(request):
@@ -102,5 +103,99 @@ def creategroup(request):
     gp.owner=request.user
     gp.title=request.user.username+"の"+request.POST["group_name"]
     gp.save()
-    message.info(request,"新しいグループを作成してください")
+    messages.info(request,"新しいグループを作成してください")
     return redirect(to="/sns/groups")
+@login_required(login_url="/admin/login/")
+def post(request):
+    if request.method=="POST":
+        gr_name=request.POST["groups"]
+        content=request.POST["content"]
+    group=Group.objects.filter(owner=request.user)\
+        .filter(title=gr_name).first()
+    if group==None:
+        (pub_user,group) = get_public()
+        msg=Message()
+        msg.owner=request.user
+        msg.group=group
+        msg.content=content
+        msg.save()
+        messages.success(request,"新しいメッセージを投稿しました")
+        return redirect(to="/sns")
+    else:
+        form=PostForm(request.user)
+    params={
+        "login_user":request.user,
+        "form":form,
+    }
+    return render(request,"sns/post.html",params)
+@login_required(login_url="admin/login/")
+def share(request,share_id):
+    share=Message.objects.get(id=share_id)
+    print(share)
+    if request.method=="POST":
+        gr_name=request.POST["groups"]
+        content=request.POST["content"]
+        group=Group.objects.filter(owner=request.user)\
+            .filter(title=gr_name).first()
+        if group==None:
+            (pub_user,group) == get_public()
+            msg=Message()
+            msg.owner=request.user
+            msg.group=group
+            msg.content=content
+            msg.share_id=share.id
+            msg.save()
+            share_msg=msg.get_share()
+            share_msg.share_count+=1
+            share_msg.save()
+            messages.success(request,"メッセージをシェアしました")
+            return redirect(to="/sns")
+        form=PostForm(request.user)
+        params={
+            "login_user":request.user,
+            "form":form,
+            "share":share,
+        }
+        return render(request,"sns/share.html",params)
+@login_required(login_url="/admin/login")
+def good(request,good_id):
+    good_msg=Message.objects.get(id=good_id)
+    is_good=Good.objects.filter(owner=request.user)\
+        .filter(message=good_msg).count()
+    if is_good>0:
+        messages.success(request,"末にメッセージにはGoodしています")
+        return redirect(to="/sns")
+    good_msg.good_count+=1
+    good_msg.save()
+    good=Good()
+    good.owner=request.user
+    good.message=good_msg
+    good.save()
+    
+    messages.success(request,"メッセージにGoodしました!")
+    return redirect(to="/sns")
+def get_your_group_message(owner,glist,page):
+    page_num=10
+    (public_user,publi_group)=get_public()
+    groups=Group.objects.filter(Q(owner=owner)\
+    |Q(owner=public_user)).filter(title__in=groups)
+    me_friends=Friend.objects.filter(group__in=groups)
+    me_user=[]
+    
+    for f in me_friends:
+        me_users.append(f.user)
+    his_groups=Group.objects.filter(owner__in=me_users)
+    his_friends=Friend.object.filter(user=owner)\
+        .filter(group__in=his_groups)
+    for hf in his_friends:
+        me_groups.append(hf.group)
+    messages=Message.objects.filter(Q(group__in=groups)\
+        |Q(group__in=me_groups))
+    page_item=Paginator(messages,page_num)
+    return page_item.get_page(page)
+
+def get_public():
+    public_user=User.objects.filter(username="public").first()
+    public_group=Group.objects.filter\
+        (owner=public_user).first()
+    return (public_user,public_group)
